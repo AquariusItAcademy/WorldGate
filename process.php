ba95752c57b898a118e3f0e0d84385c453d54069
AlaaQutfa <aquarius.it.academy@gmail.com>
2023-03-12 02:42:47 +0300

    alaa edited

--- process.php
+++ process.php
@@ -15,5 +15,3 @@
     $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
     $send = mail($to, $subject, $body, $headers);
-
-?>
