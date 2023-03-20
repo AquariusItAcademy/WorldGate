22f8bd616c17fba2d427e2bc8b76207095aa7887
AlaaQutfa <aquarius.it.academy@gmail.com>
2023-03-20 05:11:19 +0300

    first commit

---
+++ js/prodactus.js
@@ -0,0 +1,490 @@
+// Cosmetic
+// device
+//employ
+var Devices = [
+    {
+      name: "EpCare-Zenith",
+      img: "EpCare-Zenith",
+      pdf: "EpCare-Zenith",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Aleste Combi",
+      img: "AlesteCombi",
+      pdf: "AlesteCombi",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "LD2 Diode Laser",
+      img: "LD2DiodeLaser",
+      pdf: "LD2DiodeLaser",
+      type: "",
+      class: "device",
+    },
+
+    {
+      name: "Q10",
+      img: "Q10",
+      pdf: "Q10",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Cryo-Zet Air Cooling",
+      img: "Cryo-ZetAirCooling",
+      pdf: "Cryo-ZetAirCooling",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Somke evacuator(DS-2000EN)",
+      img: "Somkeevacuator",
+      pdf: "Somkeevacuator",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Smaxel co2 laser",
+      img: "Smaxelco2laser",
+      pdf: "Smaxelco2laser",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Erica",
+      img: "Erica",
+      pdf: "Erica",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Hipro-V",
+      img: "Hipro-V",
+      pdf: "Hipro-V",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Plasma bt",
+      img: "Plasmabt",
+      pdf: "Plasmabt",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Endolift Lasemar1500",
+      img: "Endolift",
+      pdf: "Endolift",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "J-Plasma",
+      img: "J-Plasma",
+      pdf: "J-Plasma",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Lipo Surg",
+      img: "LipoSurg",
+      pdf: "LipoSurg",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Orchestra Cool",
+      img: "OrchestraCool",
+      pdf: "OrchestraCool",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Ashley",
+      img: "Ashley",
+      pdf: "Ashley",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Einxelx2",
+      img: "Einxelx2",
+      pdf: "Einxelx2",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "HRZ-6000 Diode laser",
+      img: "HRZ-6000Diodelaser",
+      pdf: "HRZ-6000Diodelaser",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Hydra facial 9 in 1",
+      img: "Hydrafacial",
+      pdf: "Hydrafacial",
+      type: "",
+      class: "device",
+    },
+    {
+      name: "Med-Jet",
+      img: "Med-Jet",
+      pdf: "Med-Jet",
+      type: "",
+      class: "device",
+    },
+  ],
+  Cosmetics = [
+    {
+      name: "Botulax",
+      img: "botulax",
+      pdf: "botulax",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Meditoxin",
+      img: "medtoxin",
+      pdf: "medtoxin",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Re N Tox",
+      img: "rentox",
+      pdf: "rentox",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Angela SUB-Q",
+      img: "sub-q",
+      pdf: "sub-q",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Angela FINE",
+      img: "fine",
+      pdf: "fine",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Angela DEEP",
+      img: "deep",
+      pdf: "deep",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Anti Filler",
+      img: "Anti-Filler",
+      pdf: "Anti-Filler",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "American PRP Tube",
+      img: "PRP",
+      pdf: "PRP",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "PRP Liquid Activator",
+      img: "PRP-Liquid-Activator",
+      pdf: "PRP-Liquid-Activator",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "PRP Physical Activator",
+      img: "PRP-Physical-Activator",
+      pdf: "PRP-Physical-Activator",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Derma Pen",
+      img: "derma-pen",
+      pdf: "derma-pen",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Neo Thread Series",
+      img: "neo-thread",
+      pdf: "neo-thread",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Woods Lamp",
+      img: "Woods-Lamp",
+      pdf: "Woods-Lamp",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Lead Head Light",
+      img: "Lead-Head-Light",
+      pdf: "Lead-Head-Light",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Meso Gun",
+      img: "meso-gun",
+      pdf: "meso-gun",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Microdermabrasion",
+      img: "Microdermabrasion",
+      pdf: "Microdermabrasion",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Derma Roller",
+      img: "Derma-Roller",
+      pdf: "Derma-Roller",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Magnavision",
+      img: "Magnavision",
+      pdf: "Magnavision",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Skin Biopsy Puncher",
+      img: "Skin-Biopsy-Puncher",
+      pdf: "Skin-Biopsy-Puncher",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Spot RemovalPen",
+      img: "Spot-Removal-Pen",
+      pdf: "Spot-Removal-Pen",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Thermo Cautery",
+      img: "Thermo-Cautery",
+      pdf: "Thermo-Cautery",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Electro Cautery",
+      img: "Electro-Cautery",
+      pdf: "Electro-Cautery",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "RF Cautery",
+      img: "RF-Cautery",
+      pdf: "RF-Cautery",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Centrifuge",
+      img: "Centrifuge",
+      pdf: "Centrifuge",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Derma Scoop",
+      img: "Derma-Scoop",
+      pdf: "Derma-Scoop",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Hair Transplant instruments",
+      img: "Hair-Transplant-Instruments",
+      pdf: "Hair-Transplant-Instruments",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Cannulas",
+      img: "Cannulas",
+      pdf: "Cannulas",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Surgery Instruments",
+      img: "Surgery-Instruments",
+      pdf: "Surgery-Instruments",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Cosmo Chair",
+      img: "manual-dermatology-chair",
+      pdf: "manual-dermatology-chair",
+      type: "",
+      class: "cosmetic",
+    },
+    {
+      name: "Cosmo Chair",
+      img: "manual-dermatology-chair-02",
+      pdf: "manual-dermatology-chair",
+      type: "",
+      class: "cosmetic",
+    },
+  ],
+  employes = [],
+  maps = [
+    {
+      city: "Erbil",
+      map: "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d1648630.0597442777!2d44.030804!3d36.191354!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDExJzM2LjYiTiA0NMKwMDAnMjAuMyJF!5e0!3m2!1sen!2sus!4v1677451660712!5m2!1sen!2sus",
+      address: "Erbil, Mahmood Alhafeed Square - Qassab building",
+      number: "+964 751 235 4023",
+    },
+    {
+      city: "Baghdad",
+      map: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3333.9933630776723!2d44.338724684804156!3d33.31898898081177!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDE5JzA4LjQiTiA0NMKwMjAnMTEuNSJF!5e0!3m2!1sar!2siq!4v1677451825590!5m2!1sar!2siq",
+      address: "Baghdad, Al-Mansour, Tanque Street, Near the embassy of Venezuel",
+      number: "+964 774 443 0303",
+    },
+    {
+      city: "Sulimanya",
+      map: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3245.3008714153634!2d45.453234684744075!3d35.57096498022013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDM0JzE1LjUiTiA0NcKwMjcnMDMuOCJF!5e0!3m2!1sar!2siq!4v1677451935912!5m2!1sar!2siq",
+      address: "Sulimanya, Tomlik, Talaar bella buildung level 4",
+      number: "+964 750 852 8458",
+    },
+    {
+      city: "Dohuk",
+      map: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3192.163712779656!2d42.9923056!3d36.86249999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDUxJzQ1LjAiTiA0MsKwNTknMzIuMyJF!5e0!3m2!1sar!2siq!4v1677441898511!5m2!1sar!2siq",
+      address: "Dohuk, Nohdra, Branch opposite to first Iraq bank new building",
+      number: "+964 750 303 2672",
+    },
+    {
+      city: "Diyala",
+      map: "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3245.3008714153634!2d45.453234684744075!3d35.57096498022013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDM0JzE1LjUiTiA0NcKwMjcnMDMuOCJF!5e0!3m2!1sar!2siq!4v1677451935912!5m2!1sar!2siq",
+      address: "Diyala, Al Husseniniya Street",
+      number: "+964 772 511 1466",
+    },
+  ],
+  prodactuscontainer = document.querySelector("#Devices_Container"),
+  mapscontainer = document.querySelector("#Map_Container");
+Devices.forEach((device) => {
+  prodactuscontainer.innerHTML += `
+  <figure class="mix work-item ${device.class}">
+    <img src="img/produacts/diveces/${device.img}.png" alt="${device.img}" />
+    <figcaption class="overlay">
+      <h4>${device.name}</h4>
+      <p>${device.type}</p>
+      <ul>
+        <li>
+          <a
+            class="fancybox"
+            rel="works"
+            title="${device.name}"
+            href="img/produacts/diveces/${device.img}.png"
+            ><i class="fa fa-eye fa-lg"></i
+          ></a>
+        </li>
+        <li>
+          <a class="fancybox" rel="works" title="Order Now" href="tel:+9647512354023"
+            ><i class="fa-brands fa-whatsapp"></i
+          ></a>
+        </li>
+        <li>
+          <a
+            class="fancybox"
+            rel="works"
+            title="Download device catalog"
+            href="pdf/${device.pdf}.pdf"
+            download
+            ><i class="fa-sharp fa-solid fa-circle-down"></i
+          ></a>
+        </li>
+      </ul>
+    </figcaption>
+  </figure>
+  `;
+});
+Cosmetics.forEach((cosmetic) => {
+  prodactuscontainer.innerHTML += `
+  <figure class="mix work-item ${cosmetic.class}">
+    <img src="img/produacts/cosmatics/${cosmetic.img}.png" alt="${cosmetic.img}" />
+    <figcaption class="overlay">
+      <h4>${cosmetic.name}</h4>
+      <p>${cosmetic.type}</p>
+      <ul>
+        <li>
+          <a
+            class="fancybox"
+            rel="works"
+            title="${cosmetic.name}"
+            href="img/produacts/cosmatics/${cosmetic.img}.png"
+            ><i class="fa fa-eye fa-lg"></i
+          ></a>
+        </li>
+        <li>
+          <a class="fancybox" rel="works" title="Order Now" href="tel:+9647512354023"
+            ><i class="fa-brands fa-whatsapp"></i
+          ></a>
+        </li>
+        <li>
+          <a
+            class="fancybox"
+            rel="works"
+            title="Download device catalog"
+            href="pdf/${cosmetic.pdf}.pdf"
+            download
+            ><i class="fa-sharp fa-solid fa-circle-down"></i
+          ></a>
+        </li>
+      </ul>
+    </figcaption>
+  </figure>
+  `;
+});
+maps.forEach(map => {
+  mapscontainer.innerHTML += `
+    <div class="branch ${map.city}">
+            <div class="hover-map">
+              <iframe
+                src="${map.map}"
+                width="400"
+                height="300"
+                style="border: 0"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div class="contact-address">
+              <div class="contacte-address">
+                <p>
+                  address:
+                  <span>
+                    ${map.address}
+                  </span>
+                </p>
+                <p>
+                  Mobile:
+                  <a href="tel:${map.number}">${map.number}</a>
+                </p>
+              </div>
+            </div>
+    </div>
+  `;
+});
